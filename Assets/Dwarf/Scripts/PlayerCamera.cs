cc0491cec622101b28f3d5d8844c26e9b7c2e7a3
eduardolm <edward.lm87@gmail.com>
2014-11-22 01:20:43 +0100

    Camera

---
+++ Assets/Dwarf/Scripts/PlayerCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public SpringPosition SpringPositionComponent;
+
+    public Transform Pivot;
+
+    public float Distance = 5.43f;
+
+
+    void Update()
+    {
+        Vector3 _target = Pivot.transform.position - transform.forward * Distance;
+
+        SpringPositionComponent.target = _target;
+        SpringPositionComponent.enabled = true;
+
+        //transform.LookAt(Pivot.transform.position);
+    }
+}
