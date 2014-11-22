f13670da89b9bd22b8c17ead0c1c2d1ed2f1ec8a
rub0 <rub0te@gmail.com>
2014-11-22 15:16:13 +0100

    Axe medio implementada, sin probar

---
+++ Assets/Weapons/AxeWeapon.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AxeWeapon : Weapon
+{
+
+    void attack()
+    {
+        _attacking = true;
+    }
+
+    bool canAttack()
+    {
+        return true;
+    }
+
+
+
+	// Use this for initialization
+	void Start () {
+        selectTarget();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (weaponMode == WeaponMode.AI)
+        {
+            updateAI();
+        }
+	}
+
+    void updateAI()
+    {
+        
+
+        if(Vector3.Distance(owner.transform.position, target.transform.position) > Range)
+        {
+            move();
+        }
+        else
+        {
+            attack();
+        }
+        
+    }
+
+    void selectTarget()
+    {
+        NavMeshPath path = new NavMeshPath();
+        if (!_targetList.Contains(target))
+        {
+            foreach (GameObject pTarget in _targetList)
+            {
+                NavMesh.CalculatePath(transform.position, pTarget.transform.position, -1, path);
+
+                if (path.corners.Length < _currentPath.corners.Length && path.status != NavMeshPathStatus.PathInvalid)
+                {
+                    _currentPath = path;
+                    _currentCorner = 0;
+                }
+            }
+        }
+
+        Invoke("selectTarget", 1.0f);
+    }
+
+    void move()
+    {
+        Vector3 direction = _currentPath.corners[_currentCorner] - transform.position;
+
+        direction.y = 0;
+
+        if (direction.magnitude < 1.0)
+            direction = _currentPath.corners[++_currentCorner] - transform.position;
+
+        //owner.GetComponent<Controller>().OnInputAxis(direction);
+    }
+
+    /**
+     * Controla la colision del arma cuando esta atacando
+     * 
+    */
+    void OnCollisionEnter(Collision collision)
+    {
+        if(_attacking && !_damagedEntities.Contains(collision.gameObject))
+        {
+            //collision.gameObject.GetComponent<Life>().damage(1 + Power);
+        }
+    }
+
+    private bool _attacking;
+    private int _currentCorner;
+    private NavMeshPath _currentPath;
+    private List<GameObject> _damagedEntities;
+}
--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -1,17 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Weapon : MonoBehaviour
+public enum WeaponMode
 {
-    [System.Serializable]
-    public class AIParameters
+    CONTROLLED,
+    AI
+}
+
+public abstract class Weapon : MonoBehaviour
+{
+    abstract public void attack();
+    abstract public bool canAttack();
+
+    //IA METHODS
+    abstract public void updateAI();
+    abstract public void selectTarget();
+    abstract public void move();
+
+    public void updateTargets();
+
+    public WeaponMode weaponMode
     {
-        public int cosas = 0;
+        set
+        {
+            this._mode = value;
+        }
+        get
+        {
+            return this._mode;
+        }
     }
 
-    public string Name;
-    public int PowerBonus = 0;
-    public int AgilityBonus = 0;
+    public GameObject target
+    {
+        set
+        {
+            _currentTarget = value;
+        }
+        get
+        {
+            return _currentTarget;
+        }
+    }
 
-    public AIParameters AI = new AIParameters();
+    public GameObject owner
+    {
+        set
+        {
+            _owner = value;
+        }
+        get
+        {
+            return _owner;
+        }
+    }
+
+    public int Power
+    {
+        get
+        {
+            return _powerBonus;
+        }
+    }
+
+    public int Range
+    {
+        get
+        {
+            return _range;
+        }
+    }
+
+    [SerializeField]
+    private string _name;
+    [SerializeField]
+    private int _powerBonus = 0;
+    [SerializeField]
+    private int _agilityBonus = 0;
+    [SerializeField]
+    private int _range = 0;
+
+    private GameObject _currentTarget = null;
+    private GameObject _owner;
+    protected List<GameObject> _targetList;
+
+    private WeaponMode _mode;
+
 }
