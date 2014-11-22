1751cd1d6252ea46986426974e1b6861c1d14f7f
rub0 <rub0te@gmail.com>
2014-11-22 17:27:10 +0100

    Prefab de hacha cogible hecho, con sus componentes para ir probando. hecha la funcionalidad de coger arma, y setearle a ésta el owner como el jugador que pasó por encima (mediante un trigger). el arma se destruye al cogerla

---
+++ Assets/Weapons/PickeableWeapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickeableWeapon : MonoBehaviour {
+
+
+    public delegate void WeaponPickedHandler(GameObject who);
+    public event WeaponPickedHandler weaponPicked;
+
+	void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Controller>() == null) return;
+
+        //avisamos el player
+        if (weaponPicked != null)
+        {
+            weaponPicked(collision.gameObject);
+        }
+    }
+}
