c91e471186e877e5306a622bd7e716632c99766e
eduardolm <edward.lm87@gmail.com>
2014-11-22 12:47:54 +0100

    cosas nazis 2

--- Assets/Player/Scripts/Creatures/Stats.cs
+++ Assets/Player/Scripts/Creatures/Stats.cs
@@ -7,12 +7,13 @@ public class Stats : MonoBehaviour
     [System.Serializable]
     public class Attribute
     {
-        public int max = 0;
-        public int current = 0;
+        public int max = 2;
+        public int current = 2;
         public Attribute() { }
         public Attribute(int maxValue)
         {
             max = maxValue;
+            Regenerate();
         }
         public void Regenerate()
         {
