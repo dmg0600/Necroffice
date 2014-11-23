bd09385a8d0035394fbb6daba7c29898e90fbc4b
EduardoLM <edward.lm87@gmail.com>
2014-11-23 02:00:31 +0100

    cosas

---
+++ Assets/Scripts/RotateBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBase : MonoBehaviour
+{
+    public float Yspeed = 1;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Yspeed);
+    }
+}
